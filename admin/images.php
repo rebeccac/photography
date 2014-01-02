5f9923cf45d7450453e7249c6f2230b01fcc523f
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-01-02 19:59:18 +1030

    Image uploader finished without DB

--- admin/images.php
+++ admin/images.php
@@ -1,6 +1,6 @@
 <?php 
 	include('../config.php');
-	include($root.'includes/dtd.php');?>
+	include($root.'includes/dtd.php'); ?>
 	<title>Simon Cordingley Photography - Upload a Front Page Image</title>
 <?php
 	include($root.'includes/head.php');
@@ -10,75 +10,10 @@
 	include($root.'includes/main-content.php'); 
 ?>
 	<div class="admin-main">
-
-<?php
-
-if (isset($_POST['submit'])) {
-
-	function displayThumbnail($photo) {
-		echo '<img src="'.$photo.'" width = "200px">';
-		echo "<br><br>";
-
-	}
-
-
-
-	$allowedExts = array("jpeg", "jpg", "png");
-	$temp = explode(".", $_FILES["file"]["name"]);
-	$extension = end($temp);
-	if ((($_FILES["file"]["type"] == "image/jpeg")
-	|| ($_FILES["file"]["type"] == "image/jpg")
-	|| ($_FILES["file"]["type"] == "image/pjpeg")
-	|| ($_FILES["file"]["type"] == "image/x-png")
-	|| ($_FILES["file"]["type"] == "image/png"))
-	&& ($_FILES["file"]["size"] < 1000000)
-	&& in_array($extension, $allowedExts))
-	  {
-	  if ($_FILES["file"]["error"] > 0)
-	    {
-	    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-	    }
-	  else
-	    {
-	    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-	    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-	    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-
-	    if (file_exists("../images/frontpage/" . $_FILES["file"]["name"]))
-	      {
-	      	$photo = "../images/frontpage/".$_FILES["file"]["name"];
-	      	echo $_FILES["file"]["name"] . " already exists. ";
-	      }
-	    else
-	      {
-	      move_uploaded_file($_FILES["file"]["tmp_name"],
-	      "../images/frontpage/" . $_FILES["file"]["name"]);
-		  
-		  $photo = "../images/frontpage/".$_FILES["file"]["name"];
-	      
-	      displayThumbnail($photo);
-
-	      ?>
-	      <div>
-				<p class="admin-font"><span class="emphasis">Stored in: </span><span><?= "images/frontpage/" . $_FILES["file"]["name"]; ?></span></p>
-	      </div>
-	      <?php
-		}
-	}
-}
-	else
-	  {
-	  	echo "Invalid file";
-	  }
-
-}
-
-
-
-
-?>
+		<div class="admin-main-content">
 
 	<h2 class="admin">Upload an image to the front page</h2>
+
 		<form action=<?php echo $_SERVER['PHP_SELF']; ?>  method="post" enctype="multipart/form-data">
 			<label for="file" class="admin-font">Filename: </label>
 			<input type="file" name="file" id="file"><br>
@@ -86,8 +21,6 @@ if (isset($_POST['submit'])) {
 			<input type="text" name="alttext" id="alttext" size="60"><br>
 			<input type="submit" name="submit" value="Submit">
 		</form>
-	
-
- <?php
-include($root.'includes/footer.php');
- ?>
+<?php include('upload_file.php'); ?>
+</div>
+<?php include($root.'includes/footer.php'); ?>
--- admin/upload_file.php
+++ admin/upload_file.php
@@ -1,16 +1,6 @@
-<?php 
-	include('../config.php');
-	include($root.'includes/dtd.php');?>
-	<title>Simon Cordingley Photography - Upload a Front Page Image</title>
-	<?php
-	include($root.'includes/head.php');
-	include($root.'includes/nav.php');
-	include($root.'includes/header.php');
-	
-	include($root.'includes/main.php'); 
+<?php
 
-
-
+if (isset($_POST['submit'])) {
 
 	function displayThumbnail($photo) {
 		echo '<img src="'.$photo.'" width = "200px">';
@@ -18,9 +8,7 @@
 
 	}
 
-
-
-	$allowedExts = array("jpeg", "jpg", "png");
+	$allowedExts = array("jpeg", "jpg", "png", "JPG", "JPEG", "PNG");
 	$temp = explode(".", $_FILES["file"]["name"]);
 	$extension = end($temp);
 	if ((($_FILES["file"]["type"] == "image/jpeg")
@@ -28,7 +16,7 @@
 	|| ($_FILES["file"]["type"] == "image/pjpeg")
 	|| ($_FILES["file"]["type"] == "image/x-png")
 	|| ($_FILES["file"]["type"] == "image/png"))
-	&& ($_FILES["file"]["size"] < 1000000)
+	&& ($_FILES["file"]["size"] < 3000000)
 	&& in_array($extension, $allowedExts))
 	  {
 	  if ($_FILES["file"]["error"] > 0)
@@ -37,37 +25,60 @@
 	    }
 	  else
 	    {
-	    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-	    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-	    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+	    	$temp = $_FILES["file"]["tmp_name"];
+	    	$size = getimagesize($temp);
+	    	$width = $size[0];
+	    	$height = $size[1];
+	    	$kb = number_format(($_FILES["file"]["size"] / 1024), 2, '.', ',');
 
-	    if (file_exists("../images/frontpage/" . $_FILES["file"]["name"]))
+	    	if ($width > $height) {
+	    	$directory = 'landscape/';
+		    }
+		    else {
+		    	$directory = 'portrait/';
+		    }
+	?>
+		<h3 class="admin">File successfully uploaded</h3>
+		<p class="admin-font">
+		    <span class="bold">Upload: </span><span><?= $_FILES["file"]["name"] ?></span>
+		    <br>
+			<span class="bold">Type: </span><span><?= $_FILES["file"]["type"] ?></span>
+			<br>	    
+			<span class="bold">Size: </span><span><?= $kb . "kB" ?></span>
+ 			<br>
+		    <span class="bold">Width: </span><span><?= $width . "px" ?></span>
+		    <br>
+		    <span class="bold">Height: </span><span><?= $height . "px" ?></span>
+		    <br>
+		    <span class="bold">Stored in: </span><span><?= "images/frontpage/" .$directory . $_FILES["file"]["name"]; ?></span>
+		    <br>
+		    <br>		
+		</p>
+<?php
+
+		$photo = "../images/frontpage/" . $directory . $_FILES["file"]["name"];
+	    
+	    if (file_exists("../images/frontpage/" . $directory . $_FILES["file"]["name"]))
 	      {
-	      	$photo = "../images/frontpage/".$_FILES["file"]["name"];
 	      	echo $_FILES["file"]["name"] . " already exists. ";
 	      }
 	    else
 	      {
-	      move_uploaded_file($_FILES["file"]["tmp_name"],
-	      "../images/frontpage/" . $_FILES["file"]["name"]);
-		  
-		  $photo = "../images/frontpage/".$_FILES["file"]["name"];
-	      
-	      displayThumbnail($photo);
 
+	        move_uploaded_file($_FILES["file"]["tmp_name"], "../images/frontpage/" . $directory . $_FILES["file"]["name"]);
+	      
+	        displayThumbnail($photo);
 	      ?>
-	      <div>
-				<p><span class="emphasis">Stored in: </span><span><?= "images/frontpage/" . $_FILES["file"]["name"]; ?></span></p>
-	      </div>
-	      <?php
+	        <!-- <div>
+				<p class="admin-font"><span class="bold">Stored in: </span><span><?= "images/frontpage/" .$directory . $_FILES["file"]["name"]; ?></span></p>
+	        </div> -->
+	        <?php
 		}
 	}
 }
 	else
 	  {
-	  echo "Invalid file";
+	  	echo "Invalid file";
 	  }
-
-
-include($root.'includes/footer.php');
- ?>
+}
+?>
