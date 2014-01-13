0aeac131362046549b05e644ba7351531082aec9
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-01-13 19:44:35 +1030

    Mobile navigation

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,12 @@
-<header>
+<header class="desktop">
 	<h1>
 	Simon Cordingley Photography
+	</h1>
+</header>
+<header class="mobile">
+	<h1>
+	Simon Cordingley
+	<br>
+	Photography
 	</h1>
 </header>
